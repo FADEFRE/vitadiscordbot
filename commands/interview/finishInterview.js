7609eabb62a9cd58be5054497434e417c37689f8
Frederik Kluge <fk13mami@studserv.uni-leipzig.de>
2024-09-01 00:49:40 +0200

    first commit

---
+++ commands/interview/finishInterview.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('finish-interview')
+        .setDescription('finishes the interview and moves partner back')
+        ,
+
+    async execute(interaction) {
+
+        
+    },
+}
